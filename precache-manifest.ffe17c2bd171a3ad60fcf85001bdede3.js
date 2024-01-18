989cf594ff51c02395be8d1248b219f9d087bf52
karthikggg <karthik24003@gmail.com>
2024-01-18 18:03:54 +0530

    Updates

---
+++ precache-manifest.ffe17c2bd171a3ad60fcf85001bdede3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a45363b88f5a4900bf4b0a5bce5ef9",
+    "url": "/cheetahchug/index.html"
+  },
+  {
+    "revision": "67134bf0605a0dd15ac3",
+    "url": "/cheetahchug/static/css/main.c0e89f41.chunk.css"
+  },
+  {
+    "revision": "cec081949bce4e14559d",
+    "url": "/cheetahchug/static/js/2.e4f17a53.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/cheetahchug/static/js/2.e4f17a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612a5b222e7258a4e202",
+    "url": "/cheetahchug/static/js/3.9792cf9c.chunk.js"
+  },
+  {
+    "revision": "67134bf0605a0dd15ac3",
+    "url": "/cheetahchug/static/js/main.b1f455b4.chunk.js"
+  },
+  {
+    "revision": "b8a02fdb155c4955c06a",
+    "url": "/cheetahchug/static/js/runtime-main.b458b842.js"
+  }
+]);
